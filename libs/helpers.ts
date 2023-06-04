04e7f4af226e07a11967b86453451dff2bcf5fb0
Lukas Geppert <elrg567@gmail.com>
2023-06-04 10:02:21 -0400

    Fixed anomaly

--- libs/helpers.ts
+++ libs/helpers.ts
@@ -4,7 +4,7 @@ export const getURL = () => {
   let url =
     process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
     process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-    'https://next13-spotify-production.up.railway.app/';
+    'http://localhost:3000/';
   // Make sure to include `https://` when not localhost.
   url = url.includes('http') ? url : `https://${url}`;
   // Make sure to including trailing `/`.
